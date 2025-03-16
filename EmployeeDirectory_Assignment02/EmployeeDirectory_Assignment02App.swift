641d3d8f21a144aff10eaee34c808c5272b16ca6
Rashmi Vidushani <rashmividushani2023@gmail.com>
2025-03-16 19:04:39 -0400

    Initial Commit

---
+++ EmployeeDirectory_Assignment02/EmployeeDirectory_Assignment02App.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeDirectory_Assignment02App.swift
+//  EmployeeDirectory_Assignment02
+//
+//  Created by Rashmi Vidushani on 2025-03-16.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeDirectory_Assignment02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
